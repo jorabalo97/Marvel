31b7db938d91e1ac416752c833ed6fc600e268eb
jorabalo97 <jorabalo97@gmail.com>
2023-12-11 11:02:15 +0100

    viewcontrolerout

--- Marvel/Marvel/FCharactersList/CharactersListViewController.swift
+++ Marvel/Marvel/FCharactersList/CharactersListViewController.swift
@@ -2,73 +2,149 @@
 //  CharactersListViewController.swift
 //  Marvel
 //
-//  Created by Jorge Abalo Dieste on 4/12/23.
+//  Created by Jorge Abalo Dieste on 11/12/23.
 //
-import UIKit
 
-class CharactersListViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+import Foundation
+import UIKit
+import CryptoKit
+
+class Personaje {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class TableViewViewController: UIViewController {
     
-    var Characters = ["Iron-Man", "Capitan America", "Hulk", "Thor", "Black Widow", "Vision", "Wanda", "Spider-man", "Ojo de Halcon", "Venom", "Loki", "Capitana Marvel", "Groot", "Star Lord", "Gamora", "Rocket", "Drax", "Wolverine", "Ms.Marvel"]
+    var personajes: [Personaje] = []
+    var personajesFiltrados: [Personaje] = []
     
-    var filteredCharacters: [String]!
-    var tableView: UITableView!
-    var searchBar: UISearchBar!
+    @IBOutlet weak var tabla: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTableView()
-        setupSearchBar()
-    }
-    
-    func setupTableView() {
-        // Crear una instancia de UITableView
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(tableView)
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        
-        filteredCharacters = Characters
-    }
-    
-    func setupSearchBar() {
-        searchBar = UISearchBar()
+        // Definir los delegados de la tabla
+        // Definir el delegado del searchBar
         searchBar.delegate = self
-        navigationItem.titleView = searchBar
+        tabla.delegate = self
+        tabla.dataSource = self
+        
+        obtenerPersonajesDesdeAPI()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredCharacters.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = filteredCharacters[indexPath.row]
-        return cell
+    func obtenerPersonajesDesdeAPI() {
+        // Claves de autenticación
+        let publicKey = "4da961812496c30cf73ed692b494f315"
+        let privateKey = "d7fc2827797d7e47f6417dca83b3beeb4c5607ee"
+        
+        // Timestamp
+        let timestamp = String(Date().timeIntervalSince1970)
+        
+        // Construir el hash para la autenticación
+        let hash = "hashInput".hashed(using: Insecure.MD5.self)
+        print(hash)
+        
+        // URL de la API que proporciona los personajes
+        let baseURL = "https://gateway.marvel.com:443/v1/public/characters"
+        let limit = 20
+        let apiKeyParam = "apikey=\(publicKey)"
+        let timestampParam = "ts=\(timestamp)"
+        let hashParam = "hash=\(hash)"
+        let limitParam = "limit=\(limit)"
+        
+        let urlAPI = "\(baseURL)?\(apiKeyParam)&\(timestampParam)&\(hashParam)&\(limitParam)"
+        
+        guard let url = URL(string: urlAPI) else {
+            print("URL no válida")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            
+            if let data = data {
+                do {
+                    // Decodificar los datos JSON
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let data = json["data"] as? [String: Any],
+                       let results = data["results"] as? [[String: Any]] {
+                        
+                        for result in results {
+                            if let name = result["name"] as? String {
+                                let nuevoPersonaje = Personaje(name: name)
+                                self.personajes.append(nuevoPersonaje)
+                            }
+                        }
+                        
+                        DispatchQueue.main.async {
+                            self.tabla.reloadData()
+                        }
+                    }
+                } catch let error as DecodingError {
+                    // Manejar errores específicos de decodificación
+                    switch error {
+                    case .keyNotFound(let key, let context):
+                        print("Error de decodificación: Clave no encontrada - \(key), Contexto: \(context)")
+                    case .typeMismatch(let type, let context):
+                        print("Error de decodificación: Tipo incorrecto - \(type), Contexto: \(context)")
+                    case .valueNotFound(let value, let context):
+                        print("Error de decodificación: Valor no encontrado - \(value), Contexto: \(context)")
+                    default:
+                        print("Error de decodificación: \(error)")
+                    }
+                } catch {
+                    // Manejar otros errores
+                    print("Error: \(error)")
+                }
+            }
+        }.resume()
     }
 }
 
-extension CharactersListViewController {
+// Extensión para calcular el hash MD5
+extension String {
+    func hashed(using algorithm: any HashFunction.Type) -> String {
+        let inputData = Data(self.utf8)
+        let hashedData = algorithm.hash(data: inputData)
+        return hashedData.map { String(format: "%02hhx", $0) }.joined()
+    }
+}
+
+// Métodos SearchBar
+extension TableViewViewController: UISearchBarDelegate {
+    // Identificar cuando el usuario empieza a escribir
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        filteredCharacters = []
-        if searchText.isEmpty {
-            filteredCharacters = Characters
+        personajesFiltrados = []
+        
+        if searchText == "" {
+            personajesFiltrados = personajes
         } else {
-            for character in Characters {
-                if character.lowercased().contains(searchText.lowercased()) {
-                    filteredCharacters.append(character)
+            for personaje in personajes {
+                if personaje.name.lowercased().contains(searchText.lowercased()) {
+                    personajesFiltrados.append(personaje)
                 }
             }
         }
-        tableView.reloadData()
+        // Actualizar la tabla constantemente cuando se escriba el texto.
+        self.tabla.reloadData()
     }
 }
 
+// Métodos UITableView
+extension TableViewViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personajesFiltrados.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tabla.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        
+        celda.textLabel?.text = personajesFiltrados[indexPath.row].name
+        
+        return celda
+    }
+}
